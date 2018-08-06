90d16abcf1511e1ad5a0dbd92e883bb89c6604e7
kellygalka <kelly3000a1@gmail.com>
2018-08-06 19:21:20 +0000

    Done.

---
+++ user_input.rb
@@ -0,0 +1,6 @@
+puts "what's your name?"
+name =gets.chomp 
+puts "my name is #{name} and your watching disney channel"
+puts "what's your favorite color?"
+color =gets.chomp
+puts "#{color} is the best color in the world!"
